93dc914cd92701f64ac10d216e60f5c9e3fc0958
Rukshan Tharindu <97118309+Rukshan2000@users.noreply.github.com>
2024-05-11 04:54:16 +0530

    new

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="fixed bottom-0 w-full p-4 font-mono text-green-800 bg-black border-t border-green-950">
-      <p>© 2024 Rukshan Tharindu. All rights reserved.</p>
-      <p>Contact us: +94779054385</p>
+    <footer className="absolute bottom-0 w-full p-4 font-mono text-green-800 bg-black border-t border-green-950">
+      <div className="container mx-auto">
+        <p>© 2024 Rukshan Tharindu. All rights reserved.</p>
+        <p>Contact us: +94779054385</p>
+      </div>
     </footer>
   );
 };
