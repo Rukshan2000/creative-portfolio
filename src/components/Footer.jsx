fa00af2a5a26491b78114b1c3071e38d835bd911
Rukshan Tharindu <97118309+Rukshan2000@users.noreply.github.com>
2024-05-17 15:45:58 +0530

    test

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,16 +12,16 @@ const Footer = () => {
                     </a>
                     <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                         <li>
+                            <a href="#" className="hover:underline me-4 md:me-6">Home</a>
+                        </li>
+                        <li>
                             <a href="#" className="hover:underline me-4 md:me-6">About</a>
                         </li>
                         <li>
-                            <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
+                            <a href="#" className="hover:underline me-4 md:me-6">Projects</a>
                         </li>
                         <li>
-                            <a href="#" className="hover:underline me-4 md:me-6">Licensing</a>
-                        </li>
-                        <li>
-                            <a href="#" className="hover:underline">Contact</a>
+                            <a href="#" className="hover:underline">Blog</a>
                         </li>
                     </ul>
                     <div className="flex space-x-4">
