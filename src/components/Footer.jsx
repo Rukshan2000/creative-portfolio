47443c7253793c01936d10844d6df05500e8a8c5
Rukshan Tharindu <97118309+Rukshan2000@users.noreply.github.com>
2024-05-20 03:00:08 +0530

    navcase

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
                     </a>
                     <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                         <li className='hover:underline me-4 md:me-6'>
-                        <Link to="landing" spy={true} smooth={true} >Home</Link>
+                        <RouterLink to="/homepage" spy={true} smooth={true} >Home</RouterLink>
                         </li>
                         <li className='hover:underline me-4 md:me-6'>
                         <Link to="about" spy={true} smooth={true} >About</Link>
