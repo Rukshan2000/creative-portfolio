a9ea0b932e562d7582d69b1d675f1abf95ea7578
Rukshan Tharindu <97118309+Rukshan2000@users.noreply.github.com>
2024-05-19 19:29:27 +0530

    arrow

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,53 +5,52 @@ import About from '../components/About';
 import Projects from '../components/Projects';
 import Blog from '../components/Blog';
 import Contact from '../components/Contact';
-import { Link } from 'react-scroll';
-import Button from '../components/Button';
 import Footer from '../components/Footer';
 
 const HomePage = () => {
-    const [showTopNav, setShowTopNav] = useState(false);
-    const [showLandingPage, setShowLandingPage] = useState(false);
+  const [showTopNav, setShowTopNav] = useState(false);
+  const [showLandingPage, setShowLandingPage] = useState(false);
 
-    useEffect(() => {
-        // Set a delay of 500 milliseconds before showing the TopNav
-        const topNavTimeout = setTimeout(() => {
-            setShowTopNav(true);
-        }, 500);
+  useEffect(() => {
+    // Set a delay of 500 milliseconds before showing the TopNav
+    const topNavTimeout = setTimeout(() => {
+      setShowTopNav(true);
+    }, 500);
 
-        // Set a delay of 1000 milliseconds (1 second) before showing the LandingPage
-        const landingPageTimeout = setTimeout(() => {
-            setShowLandingPage(true);
-        }, 1000);
+    // Set a delay of 1000 milliseconds (1 second) before showing the LandingPage
+    const landingPageTimeout = setTimeout(() => {
+      setShowLandingPage(true);
+    }, 1000);
 
-        // Clear the timeouts to prevent memory leaks
-        return () => {
-            clearTimeout(topNavTimeout);
-            clearTimeout(landingPageTimeout);
-        };
-    }, []);
+    // Clear the timeouts to prevent memory leaks
+    return () => {
+      clearTimeout(topNavTimeout);
+      clearTimeout(landingPageTimeout);
+    };
+  }, []);
 
-    return (
-        <div className="bg-white">
-            <h1>WEBSITE UNDER DEVELOPMENT</h1>
-            {showTopNav && <TopNav />}
-            <section id="landing" style={{ opacity: showLandingPage ? 1 : 0, transition: 'opacity 1s' }}>
-                <LandingPage />
-            </section>
-            <section id="about">
-                <About />
-            </section>
-            <section id="projects">
-                <Projects />
-            </section>
-  
-            <section id="contact">
-                <Contact />
-            </section>
-            <Button/>
-            <Footer/>
-        </div>
-    );
+  return (
+    <div className="bg-white">
+      <h1>WEBSITE UNDER DEVELOPMENT</h1>
+      {showTopNav && <TopNav />}
+      <section id="landing" style={{ opacity: showLandingPage ? 1 : 0, transition: 'opacity 1s' }}>
+        <LandingPage />
+      </section>
+      <section id="about">
+        <About />
+      </section>
+      <section id="projects">
+        <Projects />
+      </section>
+      <section id="blog">
+        <Blog />
+      </section>
+      <section id="contact">
+        <Contact />
+      </section>
+      <Footer />
+    </div>
+  );
 };
 
 export default HomePage;
