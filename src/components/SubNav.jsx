b0df894a4f8c9541cb6f1984e541690c2e58be56
Rukshan Tharindu <97118309+Rukshan2000@users.noreply.github.com>
2024-05-20 01:39:57 +0530

    sub nav

--- src/components/SubNav.jsx
+++ src/components/SubNav.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { Link } from 'react-scroll';
 import { Link as RouterLink } from 'react-router-dom';
 
 const TopNav = () => {
@@ -37,16 +36,16 @@ const TopNav = () => {
 
       <ul className={`md:flex md:items-center md:static absolute bg-white w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-10 md:opacity-100 ${isOpen ? 'top-16 opacity-100' : 'top-full opacity-0'} justify-center`}>
         <li>
-        <RouterLink to="/homepage" className="mx-4 my-6 text-lg font-bold text-indigo-600 duration-500 hover:text-orange-300 font-Poppins md:my-0 hover:underline">Home</RouterLink>
+          <RouterLink to="/homepage" className="mx-4 my-6 text-lg font-bold text-indigo-600 duration-500 hover:text-orange-300 font-Poppins md:my-0 hover:underline">Home</RouterLink>
         </li>
         <li>
-        <RouterLink to="/homepage" className="mx-4 my-6 text-lg font-bold text-indigo-600 duration-500 hover:text-orange-300 font-Poppins md:my-0 hover:underline">About</RouterLink>
+          <RouterLink to="/homepage" className="mx-4 my-6 text-lg font-bold text-indigo-600 duration-500 hover:text-orange-300 font-Poppins md:my-0 hover:underline">About</RouterLink>
         </li>
         <li>
-        <RouterLink to="/homepage" className="mx-4 my-6 text-lg font-bold text-indigo-600 duration-500 hover:text-orange-300 font-Poppins md:my-0 hover:underline">Projects</RouterLink>
+          <RouterLink to="/homepage" className="mx-4 my-6 text-lg font-bold text-indigo-600 duration-500 hover:text-orange-300 font-Poppins md:my-0 hover:underline">Projects</RouterLink>
         </li>
         <li className="relative mx-4 my-6 text-lg font-bold text-indigo-600 duration-500 hover:text-orange-300 font-Poppins md:my-0 hover:underline">
-          <span onClick={toggleDropdown} className="cursor-pointer">Others ▼ </span>
+          <span onClick={toggleDropdown} className="cursor-pointer">Others ▼</span>
           <ul className={`absolute top-full left-0 bg-white shadow-md p-2 rounded-md mt-1 w-36 ${isDropdownOpen ? 'block' : 'hidden'}`}>
             <li className="py-1">
               <RouterLink to="/blogpage" className="text-gray-700 hover:text-indigo-600">Blog</RouterLink>
@@ -57,7 +56,7 @@ const TopNav = () => {
           </ul>
         </li>
         <li>
-        <RouterLink to="/homepage" className="mx-4 my-6 text-lg font-bold text-indigo-600 duration-500 hover:text-orange-300 font-Poppins md:my-0 hover:underline">Contact</RouterLink>
+          <RouterLink to="/homepage" className="mx-4 my-6 text-lg font-bold text-indigo-600 duration-500 hover:text-orange-300 font-Poppins md:my-0 hover:underline">Contact</RouterLink>
         </li>
       </ul>
     </nav>
