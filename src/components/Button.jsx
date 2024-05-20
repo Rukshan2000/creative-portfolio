07ee86faec304cf5c9a3131ea2610e75d152b5e7
Rukshan Tharindu <97118309+Rukshan2000@users.noreply.github.com>
2024-05-20 07:48:38 +0530

    btn

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -68,9 +68,7 @@ const Button = () => {
               <button onClick={sendMessage} className="w-full px-4 py-2 mt-4 text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none focus:bg-blue-600">
                 Send
               </button>
-              <button onClick={handlePopupClose} className="w-full px-4 py-2 mt-4 text-white bg-red-500 rounded-lg hover:bg-blue-600 focus:outline-none focus:bg-blue-600">
-                Close
-              </button>
+
             </div>
           </div>
         </div>
