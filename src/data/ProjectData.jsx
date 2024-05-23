55dac1e1c6f778f02e727d2665bc8754967580c2
Rukshan Tharindu <97118309+Rukshan2000@users.noreply.github.com>
2024-05-23 19:24:48 +0530

    new project

--- src/data/ProjectData.jsx
+++ src/data/ProjectData.jsx
@@ -8,16 +8,23 @@ export const projectData = [
         description: "We created human resourse information system for flexycode pvt lt ",
         link: "https://hris-system-nine.vercel.app/adminlogin"
     },
-    
     {
         ProjectId: 2,
+        title: "RBAS-System",
+        image: projectImage,
+        description: "This application allows users to manage systems with rolls",
+        link: "https://www.rukshantharindu.link/compilerpage"
+    },
+    
+    {
+        ProjectId: 3,
         title: "Online Code Compiler",
         image: projectImage,
         description: "This application allows users to compile html/css/js codes online and see outputs",
         link: "https://www.rukshantharindu.link/compilerpage"
     },
     {
-        ProjectId: 3,
+        ProjectId: 4,
         title: "Password Generator",
         image: projectImage,
         description: "This app allows to users create a strong random passwords",
@@ -30,4 +37,5 @@ export const projectData = [
         description: "This web application allows users to view train timetables and book a train.",
         link: "https://railway-ticket-reservation.vercel.app/"
     },
+    
 ];
